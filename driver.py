85aa09f312c18c466865027240731d982b68dfe0
Roman Potarusov <potarusov.roman@gmail.com>
2019-01-29 21:46:02 +0100

    Fix remote driver

--- driver.py
+++ driver.py
@@ -1,15 +1,15 @@
 # Manual mode
 
-import sys, time
+import sys
 import config
 
 
 class Driver:
     def __init__(self, train):
         self.train = train
-        self.moving_forward = True
+        self.moving_forward = False
         self.moving_backward = False
-        self.stop = False
+        self.stop = True
         self.obstacle_ahead = False
 
     def run(self):
@@ -24,9 +24,11 @@ class Driver:
                     self.obstacle_ahead = True
                     self.stop = True
                 elif distance_2_obstacle > 15.0 and self.obstacle_ahead and self.stop:
-                    self.train.velocity = config.min_velocity
                     self.obstacle_ahead = False
-                    self.train.move_forward(0.0)
+                    if self.moving_forward:
+                        self.train.move_forward(0.0)
+                    elif self.moving_backward:
+                        self.train.move_backward(0.0)
                     self.stop = False
 
                 btrc_frame = self.train.can.get_btrc_frame()
@@ -38,6 +40,10 @@ class Driver:
                             self.train.move_forward(config.velocity_step)
                         elif self.moving_backward:
                             self.train.move_backward(config.velocity_step)
+                        elif self.stop:
+                            self.stop = False
+                            self.moving_forward = True
+                            self.train.move_forward(0.0)
                     elif btrc_command == 'decelerate':
                         print("Decelerate")
                         if self.moving_forward:
@@ -50,21 +56,25 @@ class Driver:
                             self.moving_forward = False
                             self.moving_backward = True
                             self.train.stop()
+                            self.stop = False
                             self.train.move_backward(0.0)
                         elif self.moving_backward:
                             self.moving_backward = False
                             self.moving_forward = True
                             self.train.stop()
+                            self.stop = False
                             self.train.move_forward(0.0)
                     elif btrc_command == 'stop':
                         print("Stop")
                         self.train.stop()
+                        self.moving_forward = False
+                        self.moving_backward = False
+                        self.stop = True
                     elif btrc_command == 'shutdown':
                         print("Shutdown")
                         self.train.shutdown()
                         config.exit = 1
-                        self.train.ultrasonic.process.terminate()
-                        self.train.bluetooth.process.terminate()
+
 
 
 
